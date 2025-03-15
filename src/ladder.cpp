8f91ecae80b502c45c5e278c281fef8290650eaa
Ethan Chen <chene22@uci.edu>
2025-03-15 01:39:01 +0000

    wrote some more ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -3,6 +3,7 @@
 #include <queue>
 #include <vector>
 #include <set>
+#include <fstream>
 
 using namespace std;
 
@@ -11,11 +12,62 @@ void error(string word1, string word2, string msg){
 }
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d){
+    int first_length = str1.size();
+    int second_length = str2.size();
+    int length_diff = abs(first_length - second_length);
+    if(length_diff > d){ // length is more than allowed diff so false right away
+        return false;
+    }
+    else if(length_diff > 0){ // length differs but within d threshold, so check how many differences there are
+        int first_index = 0;
+        int second_index = 0;
+        int total_diff = 0;
 
+        const string& shorter = (first_length < second_length) ? str1 : str2;
+        const string& longer = (first_length < second_length) ? str2 : str1;
+
+        while(first_index < shorter.size() && second_index < longer.size()){
+            char first_char = tolower(shorter[first_index]);
+            char second_char = tolower(longer[second_index]);
+
+            if(first_char == second_char){
+                first_index++;
+                second_index++;
+            }
+            else{
+                ++total_diff;
+                if(total_diff > d){
+                    return false;
+                }
+                ++second_index;
+            }
+        }
+
+        // reaching here, it's possible the difference is extra letters at the end of the longer string
+        // but the while loop kicks us out once we reach the end of the shorter string
+        // so we add the difference between the last second_index and the size of the longer string
+        total_diff += abs((longer.size() - 1) - second_index)
+        return total_diff <= d;
+    }
+    else{ // reaching this means the lengths are the same
+        int char_diff = 0;
+        for(int i = 0; i < first_length; ++i){
+            char first_char = tolower(str1[i]);
+            char second_char = tolower(str2[i]);
+
+            if(first_char != second_char){
+                char_diff++;
+                if(char_diff > d){
+                    return false;
+                }
+            }
+        }
+        return char_diff <= d;
+    }
 }
 
 bool is_adjacent(const string& word1, const string& word2){
-
+    return edit_distance_within(word1, word2, 1);
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list){
@@ -46,13 +98,41 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
 }
 
 void load_words(set<string> & word_list, const string& file_name){
-
+    ifstream in(file_name);
+    string word;
+    while(in>> word){
+        for(auto &c : word){
+            c = tolower(c);
+        }
+        word_list.insert(word);
+    }
+    in.close();
 }
 
 void print_word_ladder(const vector<string>& ladder){
-
+    for(size_t i = 0; i < ladder.size(); ++i){
+        cout << ladder[i];
+        if(i < ladder.size() - 1){
+            cout << "->";
+        }
+    }
+    cout << endl;
 }
 
 void verify_word_ladder(){
+    set<string> word_list;
 
+    load_words(word_list, "words.txt");
+
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
 }
