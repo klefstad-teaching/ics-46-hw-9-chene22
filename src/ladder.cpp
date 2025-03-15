fdb3c3ab7da9e674b3d6b248c0e9d52a03733f1b
Ethan Chen <chene22@uci.edu>
2025-03-15 00:42:18 +0000

    wrote generate word ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -23,6 +23,26 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
     ladder_queue.push({begin_word});
     set<string> visited;
     visited.insert(begin_word);
+
+    while(!ladder_queue.empty()){
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder.back();
+        for(string& word : word_list){
+            if(is_adjacent(last_word, word) && visited.find(word) == visited.end()){
+                visited.insert(word);
+                vector<string> new_ladder{ladder};
+                new_ladder.push_back(word);
+                if(word == end_word){
+                    return new_ladder;
+                }
+                ladder_queue.push(new_ladder);
+            }
+        }
+    }
+
+    // at this point, no ladder found
+    return {};
 }
 
 void load_words(set<string> & word_list, const string& file_name){
